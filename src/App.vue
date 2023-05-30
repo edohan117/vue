b9fb72ab2f285736ce5946a175a55c9437a97aae
edohan <edohan0117@gmail.com>
2023-05-30 19:10:32 +0900

    testre

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,40 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Vue.js Todo List Example</title>
-  <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-</head>
-<body>
-  <div id="app">
-    <h2>Todo List</h2>
-    <input v-model="newTodo" @keyup.enter="addTodo">
-    <ul>
-      <li v-for="(todo, index) in todos" :key="index">
-        {{ todo }}
-        <button @click="removeTodo(index)">Delete</button>
-      </li>
-    </ul>
-  </div>
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
 
-  <script>
-    new Vue({
-      el: '#app',
-      data: {
-        newTodo: '',
-        todos: []
-      },
-      methods: {
-        addTodo: function() {
-          if (this.newTodo !== '') {
-            this.todos.push(this.newTodo);
-            this.newTodo = '';
-          }
-        },
-        removeTodo: function(index) {
-          this.todos.splice(index, 1);
-        }
-      }
-    });
-  </script>
-</body>
-</html>
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  </header>
+
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
